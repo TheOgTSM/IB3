ccdfe7a9b937d434381ea0426c9fee0351ff37b2
TheOgTSM <74616249+TheOgTSM@users.noreply.github.com>
2023-04-17 10:35:17 +0200

    created test function

--- signup.php
+++ signup.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST["inputPassword"];
 
     //$endMessage = SignUp($username, $password);
-    $endMessage = test();
+    $endMessage = "test successfull";
 }
 
 ?>
