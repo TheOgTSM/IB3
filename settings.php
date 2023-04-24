af7b079d6417b3ffddd8d4e312a2c006e51fe24b
TheOgTSM <74616249+TheOgTSM@users.noreply.github.com>
2023-04-24 13:56:05 +0200

    corrected redirects

--- settings.php
+++ settings.php
@@ -16,6 +16,15 @@
 
 </head>
 <body>
+
+<?php
+
+if($_SESSION['email'] = Null){
+    header("Location: /login.php");
+}
+
+?>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 
 <!-- Sidebar -->
