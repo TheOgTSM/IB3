20cae11a45d218f16831ee8cf3b821331a5b97da
TheOgTSM <74616249+TheOgTSM@users.noreply.github.com>
2023-05-22 15:25:30 +0200

    Added code for influxdb functionality

--- json/convert_to_json.py
+++ json/convert_to_json.py
@@ -2,30 +2,40 @@ from influxdb import *
 import time
 
 client = InfluxDBClient(host='localhost', port=8086)
+#look for all unique module values, this will return a list of all connected sensors
+module_query = 'SELECT distinct(module) FROM data'
+modules = client.query(module_query)
+
 
 # Query server for new results every 2 minutes to limit load on database
 while 1:
 
-    #temperature
-    query_temp = client.query('SELECT * FROM temperature ORDER BY time DESC LIMIT 100')
-    results_temp = query_temp.raw
+    for i in range(len(modules)):
+        current_module = mudules[i]
 
-    with open("temperatureResults.json", "w") as file:
-        file.write(results_temp)
+        #temperature
+        query_temp = client.query('SELECT temperature WHERE module = \'' + current_module + '\'data ORDER BY time DESC LIMIT 100')
+        results_temp = query_temp.raw
 
-    #quality
-    query_qual = client.query('SELECT * FROM airquality ORDER BY time DESC LIMIT 100')
-    results_qual = query_qual.raw
+        temp_file = 'temperatureResults' + current_module + '.json'
+        with open("temp_file", "w") as file:
+            file.write(results_temp)
 
-    with open("airQualityResults.json", "w") as file:
-        file.write(results_qual)
+        #quality
+        query_qual = client.query('SELECT airquality WHERE module = \'' + current_module + '\'data ORDER BY time DESC LIMIT 100')
+        results_qual = query_qual.raw
 
-    #light
-    query_light = client.query('SELECT * FROM light ORDER BY time DESC LIMIT 100')
-    results_light = query_light.raw
+        qual_file = 'airQualityResults' + current_module + '.json'
+        with open("qual_file", "w") as file:
+            file.write(results_qual)
 
-    with open("LightResults.json", "w") as file:
-        file.write(results_light)
+        #light
+        query_light = client.query('SELECT light WHERE module = \'' + current_module + '\'data ORDER BY time DESC LIMIT 100')
+        results_light = query_light.raw
+
+        light_file = 'LightResults' + current_module + '.json'
+        with open("LightResults.json", "w") as file:
+            file.write(results_light)
 
     #wait 2 minutes
     time.sleep(120)
